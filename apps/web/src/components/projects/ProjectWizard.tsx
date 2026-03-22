e80a91bbed97c36807e03becc4f1c3e780d64a5e
jimmy.yang <jimmy.yang@users.noreply.github.com>
2026-03-22 20:53:30 +0800

    fix: resolve file path casing issues in imports

--- apps/web/src/components/projects/ProjectWizard.tsx
+++ apps/web/src/components/projects/ProjectWizard.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useCallback } from 'react'
-import { Button } from '../ui/button'
+import { Button } from '../ui/Button'
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
